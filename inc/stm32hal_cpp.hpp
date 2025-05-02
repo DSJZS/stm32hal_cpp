0bbc3b29614bab449358062aed7959fc40496a94
CYA <3155414334@qq.com>
2025-05-02 17:37:22 +0800

    docs:包含了新内容

---
+++ inc/stm32hal_cpp.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "drivers/drivers.hpp"
+#include "interfaces/interfaces.hpp"
+#include "modlues/modlues.hpp"
+#include "protocols/protocols.hpp"
+#include "services/services.hpp"
