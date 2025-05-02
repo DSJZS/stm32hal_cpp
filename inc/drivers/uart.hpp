14089e8798e02ca6fcf79fc3bcb8865d2bbc6a02
CYA <3155414334@qq.com>
2025-05-02 17:26:25 +0800

    feat:提供了UART的C++封装

---
+++ inc/drivers/uart.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "main.h"
+
+namespace cya::halcpp::driver::uart{
+
+class Uart{
+private:
+    UART_HandleTypeDef* ptr_huart_;
+
+    static constexpr uint8_t kPrintBuffLen = 255;
+public:
+    Uart(UART_HandleTypeDef* ptr_huart);
+
+    Uart(const Uart& other_uart);
+
+    UART_HandleTypeDef* get_ptr_huart(void) const;
+
+    HAL_StatusTypeDef transmit( const uint8_t *pData,
+            uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY) const;
+
+    HAL_StatusTypeDef transmit_it( const uint8_t *pData, uint16_t Size) const;
+
+    HAL_StatusTypeDef transmit_dma( const uint8_t *pData, uint16_t Size) const;
+
+    HAL_StatusTypeDef receive( uint8_t *pData,
+                    uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY) const;
+
+    HAL_StatusTypeDef receive_it( uint8_t *pData, uint16_t Size) const;
+
+    HAL_StatusTypeDef receive_dma( uint8_t *pData, uint16_t Size) const;
+
+    HAL_StatusTypeDef receive_toIdle( uint8_t *pData,
+                        uint16_t Size, uint16_t *RxLen, uint32_t Timeout = HAL_MAX_DELAY) const;
+
+    HAL_StatusTypeDef receive_toIdle_it( uint8_t *pData, uint16_t Size) const;
+
+    HAL_StatusTypeDef receive_toIdle_dma( uint8_t *pData, uint16_t Size,
+            DMA_HandleTypeDef* ptr_hdma_usart_rx = NULL) const;
+
+    HAL_StatusTypeDef printf(const char* format, ...) const;
+
+    HAL_StatusTypeDef printf_it(const char* format, ...) const;
+
+    HAL_StatusTypeDef printf_dma(const char* format, ...) const;
+};
+
+}
---
+++ src/drivers/uart.cpp
@@ -0,0 +1,137 @@
+#include "main.h"
+#include "../../inc/drivers/uart.hpp"
+#include <stdio.h>
+#include <stdarg.h>
+#include <cstring>
+
+namespace cya::halcpp::driver::uart{
+
+Uart::Uart(UART_HandleTypeDef* ptr_huart)
+    : ptr_huart_(ptr_huart)
+{}
+
+Uart::Uart(const Uart& other_uart)
+    : ptr_huart_(other_uart.ptr_huart_)
+{}
+
+UART_HandleTypeDef* Uart::get_ptr_huart(void) const
+{
+    return this->ptr_huart_;
+}
+
+HAL_StatusTypeDef Uart::transmit( const uint8_t *pData,
+        uint16_t Size, uint32_t Timeout) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Transmit(this->ptr_huart_,pData,Size,Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive( uint8_t *pData,
+        uint16_t Size, uint32_t Timeout) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Receive(this->ptr_huart_, pData, Size, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive_it( uint8_t *pData, uint16_t Size) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Receive_IT(this->ptr_huart_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::transmit_it( const uint8_t *pData, uint16_t Size) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Transmit_IT(this->ptr_huart_,pData,Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive_dma( uint8_t *pData, uint16_t Size) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Receive_DMA(this->ptr_huart_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::transmit_dma( const uint8_t *pData, uint16_t Size) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UART_Transmit_DMA(this->ptr_huart_,pData,Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive_toIdle( uint8_t *pData,
+        uint16_t Size, uint16_t* RxLen, uint32_t Timeout) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UARTEx_ReceiveToIdle(this->ptr_huart_, pData, Size, RxLen, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive_toIdle_it( uint8_t *pData, uint16_t Size) const
+{
+    if( this->ptr_huart_ )
+        return HAL_UARTEx_ReceiveToIdle_IT(this->ptr_huart_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::receive_toIdle_dma( uint8_t *pData, uint16_t Size,
+        DMA_HandleTypeDef* ptr_hdma_usart_rx) const
+{
+    if( this->ptr_huart_ )
+    {
+        HAL_StatusTypeDef ret = HAL_OK;
+        ret = HAL_UARTEx_ReceiveToIdle_DMA(this->ptr_huart_, pData, Size);
+        if( ptr_hdma_usart_rx )
+            __HAL_DMA_DISABLE_IT( ptr_hdma_usart_rx, DMA_IT_HT);    //  关闭DMA过半传输中断
+        return ret;
+    }
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::printf(const char* format, ...) const
+{
+    if( this->ptr_huart_ )
+    {
+        char tmp_string[Uart::kPrintBuffLen] = {0};
+        va_list arg;
+        va_start( arg, format);
+        vsprintf( tmp_string, format, arg);
+        va_end(arg);
+        return this->transmit( (uint8_t*)tmp_string, strlen(tmp_string));
+    }
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::printf_it(const char* format, ...) const
+{
+    if( this->ptr_huart_ )
+    {
+        char tmp_string[Uart::kPrintBuffLen] = {0};
+        va_list arg;
+        va_start( arg, format);
+        vsprintf( tmp_string, format, arg);
+        va_end(arg);
+        return this->transmit_it( (uint8_t*)tmp_string, strlen(tmp_string));
+    }
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Uart::printf_dma(const char* format, ...) const
+{
+    if( this->ptr_huart_ )
+    {
+        char tmp_string[Uart::kPrintBuffLen] = {0};
+        va_list arg;
+        va_start( arg, format);
+        vsprintf( tmp_string, format, arg);
+        va_end(arg);
+        return this->transmit_dma( (uint8_t*)tmp_string, strlen(tmp_string));
+    }
+    return HAL_ERROR;
+}
+
+}
