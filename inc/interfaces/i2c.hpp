04c37f79ba3ca7cf191ac917b193ff22bb65c723
CYA <3155414334@qq.com>
2025-05-02 17:26:46 +0800

    feat:添加了I2C接口

---
+++ inc/interfaces/i2c.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "main.h"
+
+namespace cya::halcpp::interface{
+
+class I2C{
+public:
+    enum I2C_TRANS_DIR_{
+        I2C_DIR_TRANSMITTER_ = 0x00,
+        I2C_DIR_RECEIVER_ = 0x01
+    };
+    virtual void init(void) = 0;
+    virtual int write( uint8_t dev_addr, uint8_t reg_addr,
+        uint8_t data_len, uint8_t const* data_buf) = 0;
+    virtual int read( uint8_t dev_addr, uint8_t reg_addr,
+            uint8_t data_len, uint8_t* data_buf) = 0;
+};
+
+}
