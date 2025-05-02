6f423ce3537c92329fa834aa63304aeee51919de
CYA <3155414334@qq.com>
2025-05-02 17:25:45 +0800

    feat:提供了定时器相关功能的C++封装

---
+++ inc/drivers/tim.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "main.h"
+
+namespace cya::halcpp::driver::tim{
+
+class Base{
+protected:
+    TIM_HandleTypeDef* ptr_htim_;
+public:
+    Base(TIM_HandleTypeDef* ptr_htim, bool tim_enable = false);
+    HAL_StatusTypeDef start(void) const;
+    HAL_StatusTypeDef stop(void) const;
+    void set_counter(uint16_t tim_counter) const;
+    uint16_t get_counter(void) const;
+};
+
+/*
+class Pwm{
+private:
+    TIM_HandleTypeDef* ptr_htim_;
+    uint16_t max_speed_compare_;
+    uint16_t min_speed_compare_;
+public:
+    Pwm(TIM_HandleTypeDef* ptr_pwm_htim,
+            uint16_t max_speed_compare, uint16_t min_speed_compare = 0);
+    HAL_StatusTypeDef start(uint32_t pwm_channel) const;
+    HAL_StatusTypeDef stop(uint32_t pwm_channel) const;
+    void set_compare(uint32_t pwm_channel, float compare_value) const;
+};
+*/
+
+class Pwm_Channel{
+protected:
+    TIM_HandleTypeDef* ptr_htim_;
+    uint32_t pwm_channel_;
+    uint16_t max_speed_compare_;
+    uint16_t min_speed_compare_;
+public:
+    Pwm_Channel(TIM_HandleTypeDef* ptr_htim_,uint32_t pwm_channel,
+            uint16_t max_speed_compare, uint16_t min_speed_compare = 0,
+            bool pwm_enable = false);
+    HAL_StatusTypeDef start(void) const;
+    HAL_StatusTypeDef stop(void) const;
+    void set_compare(float compare_value) const;
+};
+
+class Encoder{
+protected:
+    TIM_HandleTypeDef* ptr_htim_;
+    uint32_t encoder_channel_;
+public:
+    Encoder(TIM_HandleTypeDef* ptr_encoder_htim,
+            uint32_t encoder_channel = TIM_CHANNEL_ALL,
+            bool enable_enable = false);
+    HAL_StatusTypeDef start(void) const;
+    HAL_StatusTypeDef stop(void) const;
+    int16_t get_count_1fm(void) const;
+    int16_t get_count_2fm(void) const;
+    float get_count_2fm_f(void) const;
+    int16_t get_count_4fm(void) const;
+    float get_count_4fm_f(void) const;
+};
+
+}
---
+++ src/drivers/tim.cpp
@@ -0,0 +1,125 @@
+#include "main.h"
+#include "../../inc/drivers/tim.hpp"
+
+namespace cya::halcpp::driver::tim{
+
+Base::Base(TIM_HandleTypeDef* ptr_htim, bool tim_enable)
+    : ptr_htim_(ptr_htim)
+{
+    if(tim_enable && this->ptr_htim_)
+        this->start();
+}
+
+HAL_StatusTypeDef Base::start(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_Base_Start(this->ptr_htim_);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Base::stop(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_Base_Stop(this->ptr_htim_);
+    return HAL_ERROR;
+}
+
+uint16_t Base::get_counter(void) const
+{
+    if( this->ptr_htim_ )
+        return __HAL_TIM_GET_COUNTER(this->ptr_htim_);
+    return 0;
+}
+
+void Base::set_counter(uint16_t tim_counter) const
+{
+    if( this->ptr_htim_ )
+        __HAL_TIM_SET_COUNTER(this->ptr_htim_, tim_counter);
+}
+
+Pwm_Channel::Pwm_Channel(
+        TIM_HandleTypeDef* ptr_htim,uint32_t pwm_channel,
+        uint16_t max_speed_compare, uint16_t min_speed_compare,
+        bool pwm_enable)
+    : ptr_htim_(ptr_htim), pwm_channel_(pwm_channel),
+      max_speed_compare_(max_speed_compare),min_speed_compare_(min_speed_compare)
+{
+    if( pwm_enable && this->ptr_htim_ )
+        this->start();
+}
+
+HAL_StatusTypeDef Pwm_Channel::start(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_PWM_Start(this->ptr_htim_, this->pwm_channel_);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Pwm_Channel::stop(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_PWM_Stop(this->ptr_htim_, this->pwm_channel_);
+    return HAL_ERROR;
+}
+
+void Pwm_Channel::set_compare(float compare_value) const
+{
+    if( this->ptr_htim_ )
+        __HAL_TIM_SET_COMPARE(this->ptr_htim_,this->pwm_channel_,
+            compare_value * ( this->max_speed_compare_ - this->min_speed_compare_));
+}
+
+Encoder::Encoder(TIM_HandleTypeDef* ptr_htim,
+        uint32_t encoder_channel, bool enable_enable)
+    : ptr_htim_(ptr_htim), encoder_channel_(encoder_channel)
+{
+    if( enable_enable && this->ptr_htim_ )
+        HAL_TIM_Encoder_Start(this->ptr_htim_, this->encoder_channel_);
+}
+
+HAL_StatusTypeDef Encoder::start(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_Encoder_Start(this->ptr_htim_, this->encoder_channel_);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Encoder::stop(void) const
+{
+    if( this->ptr_htim_ )
+        return HAL_TIM_Encoder_Stop(this->ptr_htim_, this->encoder_channel_);
+    return HAL_ERROR;
+}
+
+int16_t Encoder::get_count_1fm(void) const
+{
+    if( this->ptr_htim_ )
+    {
+        int16_t encoder_counter = (int16_t)( __HAL_TIM_GET_COUNTER(this->ptr_htim_) );
+        __HAL_TIM_SET_COUNTER( this->ptr_htim_, 0);
+        return encoder_counter;
+    }
+    return 0;
+}
+
+int16_t Encoder::get_count_2fm(void) const
+{
+    return this->get_count_1fm() / 2;
+}
+
+float Encoder::get_count_2fm_f(void) const
+{
+    return (float)(this->get_count_1fm()) / 2.0f;
+}
+
+int16_t Encoder::get_count_4fm(void) const
+{
+    return this->get_count_1fm() / 4;
+}
+
+float Encoder::get_count_4fm_f(void) const
+{
+    return (float)(this->get_count_1fm()) / 4.0f;
+}
+
+}
