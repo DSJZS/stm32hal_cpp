84faf4f46ba3ff1b619a1a058a1b466e8368fa1b
CYA <3155414334@qq.com>
2025-05-02 17:30:38 +0800

    feat:提供了蓝牙接口

---
+++ inc/modlues/bluetooth.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace cya::halcpp::modlues{
+
+class Blue_Tooth{
+public:
+    virtual void init(void) = 0;
+    virtual void send_data(uint8_t* data, uint16_t size) = 0;
+};
+
+}
