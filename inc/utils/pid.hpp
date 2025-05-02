a63e761691749511065a0f499177d90267f4eb4e
CYA <3155414334@qq.com>
2025-05-02 17:28:00 +0800

    feat:封装了PID算法

---
+++ inc/utils/pid.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "main.h"
+
+namespace cya::halcpp::utils{
+
+class pid{
+private:
+    bool float_abs_limit( float* num, float max);
+    float error_;            //  本次误差
+    float last_error_;       //  上次误差
+public:
+    float target_;           //  目标值
+    float current_;          //  当前值
+    float kp_;               //  Kp参数
+    float ki_;               //  Ki参数
+    float kd_;               //  Kd参数
+    float p_out_;          //  比例输出
+    float i_out_;          //  积分输出
+    float d_out_;          //  微分输出
+    float total_out_;      //  总输出
+    float total_out_limit_;//  总输出最大值
+    float i_band_;        //  积分分离
+    float i_limit_;       //  积分限幅
+    pid(float kp,float ki,float kd,
+            float total_out_limit = 1.0,
+            float i_band = 1.0,
+            float i_limit = 1.0);
+    void set_pid(float kp ,float ki,float kd);
+    void set_limit(float total_out_limit,float i_limit,float i_band);
+    float calc_output( float target, float current);
+    void stop_output(void);
+};
+
+}
---
+++ src/utils/pid.cpp
@@ -0,0 +1,88 @@
+/***************************************************************************************
+* 本源文件含有PID运算相关函数
+***************************************************************************************
+*/
+
+#include "main.h"
+
+#include "../../inc/utils/PID.hpp"
+#include <cmath>
+#include "main.h"
+
+namespace cya::halcpp::utils{
+
+
+bool pid::float_abs_limit( float* num, float max)
+{
+    float x = *num;
+    if (max < 0)
+        return false;  // 处理无效的max值
+
+    float abs_x = fabs(x);
+    *num = (abs_x > max) ? ((x < 0) ? -max : max) : x;
+    return true;
+}
+
+pid::pid(float kp,float ki,float kd,
+        float total_out_limit,float i_band,float i_limit)
+    : error_(0.0f), last_error_(0.0f), target_(0.0f), current_(0.0f),
+      kp_(kp), ki_(ki), kd_(kd), p_out_(0.0f), i_out_(0.0f), d_out_(0.0f), total_out_(0.0f),
+      total_out_limit_(total_out_limit), i_band_(i_band), i_limit_(i_limit)
+{}
+
+void pid::set_pid(float kp ,float ki,float kd)
+{
+    this->kp_ = kp;
+    this->ki_ = ki;
+    this->kd_ = kd;
+}
+
+void pid::set_limit(float total_out_limit,float i_limit,float i_band)
+{
+    this->total_out_limit_ = total_out_limit;
+    this->i_limit_ = i_limit;
+    this->i_band_ = i_band;
+}
+
+float pid::calc_output( float target, float current)
+{
+    this->target_ = target;
+    this->current_ = current;
+
+    this->error_ = this->target_ - this->current_;            //  得出本次误差
+
+    this->p_out_ = ( this->kp_ * this->error_ );     //  通过本次误差和上次误差得出比例输出
+
+    if( fabsf(this->error_) < this->i_band_)                //  判断是否要进行积分分离
+    {
+        this->i_out_ += ( this->ki_ * this->error_ );//  积分累计误差
+
+        this->float_abs_limit( &(this->i_out_), this->i_limit_ );       //  判断是否要进行积分限幅
+    }
+    else                                                //  如果要进行积分分离
+    {
+        this->i_out_ = 0;                                 //  清零积分输出
+    }
+
+    this->d_out_ = ( this->kd_ * ( this->error_ - this->last_error_ ) );   //  通过本次误差和上次误差得出微分输出
+
+    this->total_out_ = this->p_out_ + this->i_out_ + this->d_out_;                  //  通过比例输出、积分输出、微分输出得出总输出
+
+    this->float_abs_limit( &(this->total_out_), this->total_out_limit_ );                   //  判断总输出是否过大，对其进行限制
+
+    this->last_error_ = this->error_;                                           //  将本次输出保存到上次输出变量以供下一次运算
+
+    return this->total_out_;
+}
+
+void pid::stop_output(void)
+{
+    this->error_ = 0;
+    this->last_error_ = 0;
+    this->target_ = 0;
+    this->p_out_ = 0;
+    this->i_out_ = 0;
+    this->d_out_ = 0;
+}
+
+}
