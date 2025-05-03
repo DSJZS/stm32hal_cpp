e5501cd9cacb05a98434ed92a4cf274cc67ca06d
CYA <3155414334@qq.com>
2025-05-03 19:00:34 +0800

    feat:封装了单次连续缓存

---
+++ inc/utils/single_shot_receiver.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "main.h"
+#include "buffer_receiver.hpp"
+
+namespace cya::halcpp::utils{
+
+typedef class Low_Speed_Data_Rx : public Buffer_Receiver
+{
+public:
+    uint16_t read_ptr_;
+    uint16_t write_ptr_;
+    void write_byte(uint8_t byte);
+public:
+    Low_Speed_Data_Rx( uint8_t* rx_buffer, uint16_t capacity);
+
+    //  写入数据
+    virtual bool write(uint8_t* data, uint16_t size) override;
+    //  读出数据，不指定 size 默认全读( 读取数据不等于删除数据 )
+    virtual uint16_t read(uint8_t* data, uint16_t size = 0) override;
+    //  删除数据，不指定 size 默认全删
+    virtual uint16_t clear(uint16_t size ) override;
+    //  获取缓冲区当前有多少字节数据
+    virtual uint16_t length(void) override;
+    //  获取缓冲区剩余空位
+    virtual uint16_t remain(void) override;
+}Single_Shot_Receiver;
+
+}
---
+++ src/utils/single_shot_receiver.cpp
@@ -0,0 +1,87 @@
+#include "main.h"
+
+#include "../../inc/utils/single_shot_receiver.hpp"
+#include <cstring>
+
+namespace cya::halcpp::utils{
+
+Low_Speed_Data_Rx::Low_Speed_Data_Rx( uint8_t* rx_buffer, uint16_t capacity)
+    : read_ptr_(0), write_ptr_(0)
+{
+    this->rx_buffer_ = rx_buffer;
+    this->capacity_ = capacity;
+}
+
+void Low_Speed_Data_Rx::write_byte(uint8_t byte)
+{
+    this->rx_buffer_[this->write_ptr_++] = byte;
+}
+
+
+uint16_t Low_Speed_Data_Rx::length(void)
+{
+    return ( this->write_ptr_ - this->read_ptr_ );
+}
+
+bool Low_Speed_Data_Rx::write(uint8_t* data, uint16_t size)
+{
+    if( size == 0 )
+        return false;
+
+    if( data == NULL )
+        return false;
+
+    if( size > this->remain() )
+    {
+        this->clear( this->length() );
+        return false;
+    }
+
+    for( int i = 0 ; i < size ; ++i )
+    {
+        this->write_byte(data[i]);
+    }
+    return false;
+}
+
+uint16_t Low_Speed_Data_Rx::read(uint8_t* data, uint16_t size)
+{
+    if( data == NULL)
+        return 0;
+    if( size > this->length() )
+        return 0;
+
+    if( size == 0)
+    {
+        size = this->length();
+    }
+
+    for( int i = this->read_ptr_ ; i < this->read_ptr_ + size ; ++i )
+    {
+        data[ i - this->read_ptr_ ] = this->rx_buffer_[ i ];
+    }
+    return size;
+}
+
+uint16_t Low_Speed_Data_Rx::clear(uint16_t size)
+{
+    uint16_t len = this->length();
+    if( size >= len )
+    {
+        this->read_ptr_ = 0;
+        this->write_ptr_ = 0;
+        return len;
+    } else {
+        this->read_ptr_ += size;
+        return size;
+    }
+}
+
+uint16_t Low_Speed_Data_Rx::remain(void)
+{
+    return ( this->capacity_ - this->write_ptr_ );
+}
+
+
+
+}
