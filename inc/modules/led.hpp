f18edd7bbc7ba31a08a983fc182b47c7bca3a82a
CYA <3155414334@qq.com>
2025-06-04 14:24:15 +0800

    feat:封装了单色LED

---
+++ inc/modules/led.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../peripherals/gpio.hpp"
+
+namespace cya::module{
+
+class Led{
+public:
+    enum class ActiveLevel{HIGH,LOW};
+    enum class State{ON,OFF};
+
+    Led( peripheral::gpio::Pin& pin, Led::ActiveLevel level);
+    Led( GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, Led::ActiveLevel level);
+    ~Led();
+
+    void on(void);
+    void off(void);
+    void toggle(void);
+    Led::State state(void);
+private:
+    peripheral::gpio::Pin pin_;
+    Led::ActiveLevel level_;
+};
+
+}
---
+++ src/modules/led.cpp
@@ -0,0 +1,55 @@
+#include "main.h"
+#include "../../inc/modules/led.hpp"
+
+namespace cya::module{
+
+Led::Led(peripheral::gpio::Pin& pin, Led::ActiveLevel level)
+    : pin_(pin), level_(level)
+{
+    this->off();
+}
+
+Led::Led(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, Led::ActiveLevel level)
+    : pin_( GPIOx, GPIO_Pin), level_(level)
+{
+    this->off();
+}
+
+Led::~Led()
+{
+    this->off();
+}
+
+void Led::on(void)
+{
+    if( this->level_ == ActiveLevel::HIGH )
+        this->pin_.set();
+    else
+        this->pin_.reset();
+}
+
+void Led::off(void)
+{
+    if( this->level_ == ActiveLevel::HIGH )
+        this->pin_.reset();
+    else
+        this->pin_.set();
+}
+
+void Led::toggle(void)
+{
+    this->pin_.toggle();
+}
+
+Led::State Led::state(void)
+{
+    uint8_t pin_state = this->pin_.read();
+    if( ( this->level_ == ActiveLevel::HIGH ) && ( pin_state != 0 ) )
+        return Led::State::ON;
+    else if( ( this->level_ == ActiveLevel::LOW ) && ( pin_state == 0 ) )
+        return Led::State::ON;
+    else
+        return Led::State::OFF;
+}
+
+}
