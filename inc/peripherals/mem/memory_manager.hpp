6f07d924a7e1fedee27cbea8d0119db5bfd5f8ae
CYA <3155414334@qq.com>
2025-06-04 13:02:34 +0800

    refactor:添加了更多的宏来管理

--- inc/peripherals/mem/memory_manager.hpp
+++ inc/peripherals/mem/memory_manager.hpp
@@ -2,4 +2,12 @@
 
 #include "../../../inc/peripherals/mem/mem_manager.h"
 
-#define MEM_MANAGER_DMP_CAPABLE     (0)
+#define __ENABLE_MEM_MANGER__
+
+#ifdef __ENABLE_MEM_MANGER__
+
+#ifndef MEM_MANAGER_DMP_CAPABLE
+    #define MEM_MANAGER_DMP_CAPABLE     (0)
+#endif
+
+#endif
