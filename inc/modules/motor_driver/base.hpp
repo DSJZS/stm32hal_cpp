a24e772f0b93f39de1026c305492984cf459483b
CYA <3155414334@qq.com>
2025-06-08 19:35:06 +0800

    refactor:删除电机驱动接口的多余方法

--- inc/modules/motor_driver/base.hpp
+++ inc/modules/motor_driver/base.hpp
@@ -7,15 +7,12 @@ namespace cya::module::motor_driver{
 class Base{
 public:
     virtual void init(void) = 0;
-    virtual void forward(void) const = 0;
-    virtual void back(void) const = 0;
-
+    
     //  按照百分比设定速度与方向，为 -1 ~ +1
     //  正则正转，反则反转，绝对值越大转的越快
-    virtual int16_t set_speed(float base_speed = 0.0f) const = 0;
+    //  为 0 时停止转动
+    virtual void set_speed(float base_speed = 0.0f) const = 0;
 
-    virtual void bark(void) const = 0;
-    virtual void stop(void) const = 0;
 };
 
 }
