b24c9e06354cf02b7257d9310aa353fe8b5f56f2
CYA <3155414334@qq.com>
2025-06-08 19:32:17 +0800

    style:修改了格式

--- inc/interfaces/i2c_interface.hpp
+++ inc/interfaces/i2c_interface.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "main.h"
+#include <stdint.h>
 
 namespace cya::interface{
 
--- inc/protocols/soft_i2c.hpp
+++ inc/protocols/soft_i2c.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "main.h"
 #include "../interfaces/i2c_interface.hpp"
 #include "../peripherals/gpio.hpp"
 
