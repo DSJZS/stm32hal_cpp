a03a0888af2f86d6b2dd351911f6e9998f98e255
CYA <3155414334@qq.com>
2025-06-10 19:07:40 +0800

    feat:添加了步进电机驱动的定位功能

--- inc/modules/motor_driver/dm542c.hpp
+++ inc/modules/motor_driver/dm542c.hpp
@@ -17,38 +17,74 @@ public:
         CW,
         CCW,
     };
-    Dm542c( Dm542c::ConnectType ct, const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR );
+    Dm542c( Dm542c::ConnectType ct, uint16_t microsteps, const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR );
 protected:
     Dm542c::ConnectType ct_;
+    uint16_t microsteps_;
+
     const peripheral::gpio::Pin EN_;
     const peripheral::gpio::Pin DIR_;
 
     void set_enable( uint8_t enable) const;
     void set_dire( Dm542c::DireType dire) const;
+
+    virtual void set_angle(float angle) const = 0;
 };
 
 class Dm542c_Pin : public Dm542c{
 private:
     const peripheral::gpio::Pin PULSE_;
 public:
-    Dm542c_Pin( Dm542c::ConnectType ct,
+    Dm542c_Pin( Dm542c::ConnectType ct, uint16_t microsteps,
         const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR,
         const peripheral::gpio::Pin& PULSE);
 
     virtual void init(void) override;
     virtual void set_speed(float base_speed = 0.0f) const override;
+    virtual void set_angle(float angle) const override;
 };
 
+/* 如果要使用 set_angle 方法, 门空配置方法
+ * TIMx 输出PWM，TIMy 产生门信号
+ * 
+ * TIMx 配置: 
+ * Slave Mode : Gated Mode
+ * Trigger Mode : ITRy ( 指向 TIMy )
+ * Clock Source : Internal Clock
+ * ARR : 调节脉冲频率
+ * CCR : = ARR >> 1
+ * TRGO : Update Event
+ * Channelx : PWM Generation CHx
+ * 按照 PWM 配置
+ * 
+ * TIMy 配置: 
+ * Slave Mode : External Clock Mode 1
+ * Trigger Mode : ITRx ( 指向 TIMx )
+ * Clock Source : Disable
+ * ARR : 尽可能大, 增大可操作范围
+ * CCR : 调节目标脉冲数
+ * TRGO : OCxREF
+ * Channelx : Output Compare No Output
+ * 
+ * 
+ * Slave TIM        ITR0    ITR1    ITR2    ITR3
+ * 
+ */
 class Dm542c_Pwm : public Dm542c{
 private:
-    const peripheral::tim::Pwm_Channel PULSE_;
+    const peripheral::tim::Pwm_Channel PULSE_;  //  PWM通道  
+    const peripheral::tim::Pwm_Channel GATE_;   //  门信号通道
+
+    void set_step(uint16_t step) const;
 public:
-    Dm542c_Pwm(Dm542c::ConnectType ct,
+    Dm542c_Pwm(Dm542c::ConnectType ct, uint16_t microsteps,
         const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR,
-        const peripheral::tim::Pwm_Channel& PULSE);
+        const peripheral::tim::Pwm_Channel& PULSE,
+        const peripheral::tim::Pwm_Channel& GATE);
 
     virtual void init(void) override;
     virtual void set_speed(float base_speed = 0.0f) const override;
+    virtual void set_angle(float angle) const override;
 };
 
 }
--- src/modules/motor_driver/dm542c.cpp
+++ src/modules/motor_driver/dm542c.cpp
@@ -4,8 +4,9 @@
 
 namespace cya::module::motor_driver{
 
-Dm542c::Dm542c( Dm542c::ConnectType ct, const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR )
-    : ct_(ct), EN_(EN), DIR_(DIR)
+Dm542c::Dm542c( Dm542c::ConnectType ct, uint16_t microsteps, 
+    const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR )
+    : ct_(ct), microsteps_(microsteps), EN_(EN), DIR_(DIR)
 {}
 
 void Dm542c::set_enable(uint8_t enable) const
@@ -44,10 +45,10 @@ void Dm542c::set_dire( Dm542c::DireType dire) const
 
 /********************************************************************************************* */
 
-Dm542c_Pin::Dm542c_Pin(Dm542c::ConnectType ct,
+Dm542c_Pin::Dm542c_Pin(Dm542c::ConnectType ct, uint16_t microsteps,
     const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR,
     const peripheral::gpio::Pin& PULSE)
-    : Dm542c( ct, EN, DIR), PULSE_(PULSE)
+    : Dm542c( ct, microsteps, EN, DIR), PULSE_(PULSE)
 {
     this->init();
 }
@@ -64,10 +65,11 @@ void Dm542c_Pin::init(void)
 /********************************************************************************************* */
 
 
-Dm542c_Pwm::Dm542c_Pwm(Dm542c::ConnectType ct,
+Dm542c_Pwm::Dm542c_Pwm(Dm542c::ConnectType ct, uint16_t microsteps,
     const peripheral::gpio::Pin& EN, const peripheral::gpio::Pin& DIR,
-    const peripheral::tim::Pwm_Channel& PULSE)
-    : Dm542c( ct, EN, DIR), PULSE_(PULSE)
+    const peripheral::tim::Pwm_Channel& PULSE,
+    const peripheral::tim::Pwm_Channel& GATE)
+    : Dm542c( ct, microsteps, EN, DIR), PULSE_(PULSE), GATE_(GATE)
 {
     this->init();
 }
@@ -77,6 +79,29 @@ void Dm542c_Pwm::init(void)
     this->set_enable(0);
     this->set_dire(Dm542c::DireType::CW);
     this->PULSE_.stop();
+}
+
+void Dm542c_Pwm::set_step(uint16_t step) const
+{
+    this->set_enable(0);
+    
+    this->PULSE_.stop();
+    // this->GATE_.stop();
+
+    this->PULSE_.set_cnt(0);
+    this->GATE_.set_cnt(0);
+    this->GATE_.set_compare(step);
+
+    this->set_enable(1);
+
+    this->PULSE_.start();
+    // this->GATE_.start();
+}
+
+// step = angle / ( 1.8 / microsteps )
+void Dm542c_Pwm::set_angle(float angle) const
+{
+    this->set_step( (uint16_t)( angle * this->microsteps_ / 1.8 )  );
 }
 
 }
