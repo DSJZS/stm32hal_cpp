3d12138a46884ac06f6fa0ed97940be026bc6238
CYA <3155414334@qq.com>
2025-06-26 16:03:14 +0800

    refactor:添加了头文件引用

--- inc/modules/modules.hpp
+++ inc/modules/modules.hpp
@@ -3,7 +3,7 @@
 //实现独立功能模块，封装具体业务逻辑或硬件功能。
 
 /*  硬件功能    */
-#include "./motor_driver/tb6612.hpp"
+#include "./motor_driver/motor_driver.hpp"
 
 /*  业务逻辑    */
 #include "func_runtime.hpp"
---
+++ inc/modules/motor_driver/motor_driver.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "./tb6612.hpp"
+#include "./dm542c.hpp"
