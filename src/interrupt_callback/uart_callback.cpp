10f85f0f1bd6ab3f69a121314c77d1b1c29d609b
CYA <3155414334@qq.com>
2025-07-05 17:53:20 +0800

    feat:串口回调模板

--- src/interrupt_callback/uart_callback.cpp
+++ src/interrupt_callback/uart_callback.cpp
@@ -2,8 +2,10 @@
 #include "../inc/interrupt_callback/uart_callback.hpp"
 
 #include "main.h"
+#include "stm32hal_cpp.hpp"
 /* Exported types ------------------------------------------------------------*/
-
+extern cya::peripheral::Uart debug_uart;
+extern uint8_t debug_data[256];
 /* Exported constants --------------------------------------------------------*/
 
 /* Exported macro ------------------------------------------------------------*/
@@ -18,14 +20,17 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
-
+    if( huart == debug_uart.handle() )
+    {
+        debug_uart.receive_toIdle_dma( debug_data, sizeof(debug_data), &hdma_usart1_rx );
+    }
 }
 
 //  串口错误
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 {
-//    if( huart == debug_uart.handle() )
-//    {
-//        debug_uart.receive_toIdle_dma( debug_data, sizeof(debug_data), &hdma_usart1_rx );
-//    }
+   if( huart == debug_uart.handle() )
+   {
+       debug_uart.receive_toIdle_dma( debug_data, sizeof(debug_data), &hdma_usart1_rx );
+   }
 }
