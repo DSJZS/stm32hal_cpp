b2e0ca44e4cc8453036dce1a115e32060526213e
CYA <3155414334@qq.com>
2025-07-07 18:44:35 +0800

    opt:优化了串口回调模板

--- src/interrupt_callback/uart_callback.cpp
+++ src/interrupt_callback/uart_callback.cpp
@@ -12,16 +12,28 @@ extern uint8_t debug_data[256];
 
 /* Exported functions ------------------------------------------------------- */
 
+cya::protocol::Simple_Frame_Parser sfp('@');
+cya::utils::Cyclic_Buffer_Receiver cyc_buffer( debug_data, 256);
+uint8_t command[256] = {0};
+cya::protocol::Parser_IO pio = { &cyc_buffer, command, 0, nullptr};
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-
+    if( huart == debug_uart.handle() )
+    {
+        debug_uart.receive_dma( debug_data, sizeof(debug_data));
+    }
 }
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
     if( huart == debug_uart.handle() )
     {
+        cyc_buffer.write( debug_data, Size);
+        if( sfp.get_command(&pio) )
+        {
+            __NOP();
+        }
         debug_uart.receive_toIdle_dma( debug_data, sizeof(debug_data));
     }
 }
