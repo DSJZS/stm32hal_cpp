8ad384d583e940c1afbab6c4b8c704324b7b4259
CYA <3155414334@qq.com>
2025-07-14 14:28:52 +0800

    feat:添加了硬件I2C基础功能

--- inc/peripherals/hard_i2c.hpp
+++ inc/peripherals/hard_i2c.hpp
@@ -7,16 +7,31 @@ namespace cya::peripheral{
 
 class Hard_I2C : public interface::I2C_Interface{
 private:
-    I2C_HandleTypeDef *hi2c_;
+    I2C_HandleTypeDef* ptr_hi2c_;
 public:
-    Hard_I2C( I2C_HandleTypeDef *hi2c );
+    Hard_I2C( I2C_HandleTypeDef* ptr_hi2c);
     Hard_I2C( const Hard_I2C& other);
     Hard_I2C( Hard_I2C&& other);
-    
+
+    HAL_StatusTypeDef master_transmit( uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY)
+    HAL_StatusTypeDef master_receive( uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY);
+    HAL_StatusTypeDef slave_transmit( uint8_t *pData, uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY);
+    HAL_StatusTypeDef slave_receive( uint8_t *pData, uint16_t Size, uint32_t Timeout = HAL_MAX_DELAY);
+
+    HAL_StatusTypeDef master_transmit_it( uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef master_receive_it( uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef slave_transmit_it( uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef slave_receive_it( uint8_t *pData, uint16_t Size);
+
+    HAL_StatusTypeDef master_transmit_dma( uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef master_receive_dma( uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef slave_transmit_dma( uint8_t *pData, uint16_t Size);
+    HAL_StatusTypeDef slave_receive_dma( uint8_t *pData, uint16_t Size);
+
     virtual void init(void) override;
-    virtual int write( uint8_t dev_addr, uint8_t reg_addr,
+    virtual uint8_t write( uint8_t dev_addr, uint8_t reg_addr,
             uint8_t data_len, uint8_t const* data_buf) override;
-    virtual int read( uint8_t dev_addr, uint8_t reg_addr,
+    virtual uint8_t read( uint8_t dev_addr, uint8_t reg_addr,
             uint8_t data_len, uint8_t* data_buf) override;
 };
 
--- src/peripherals/hard_i2c.cpp
+++ src/peripherals/hard_i2c.cpp
@@ -4,36 +4,128 @@
 namespace cya::peripheral{
 
 Hard_I2C::Hard_I2C( I2C_HandleTypeDef *hi2c )
-    : hi2c_(hi2c)
-{}
+    : ptr_hi2c_(ptr_hi2c)
+{
+    this->init();
+}
 
 Hard_I2C::Hard_I2C( const Hard_I2C& other)
-    : hi2c_(other.hi2c_)
+    : ptr_hi2c_(other.ptr_hi2c_)
 {}
 
 Hard_I2C::Hard_I2C( Hard_I2C&& other)
-    : hi2c_(other.hi2c_)
+    : ptr_hi2c_(other.ptr_hi2c_)
 {
-    other.hi2c_ = nullptr;
+    other.ptr_hi2c_ = nullptr;
 }
+
+HAL_StatusTypeDef Hard_I2C::master_transmit( uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Transmit( this->ptr_hi2c_, DevAddress, pData, Size, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::master_receive( uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Receive( this->ptr_hi2c_, DevAddress, pData, Size, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_transmit( uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Transmit( this->ptr_hi2c_, pData, Size, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_receive( uint8_t *pData, uint16_t Size, uint32_t Timeout)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Receive( this->ptr_hi2c_, pData, Size, Timeout);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::master_transmit_it( uint16_t DevAddress, uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Transmit_IT( this->ptr_hi2c_, DevAddress, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::master_receive_it( uint16_t DevAddress, uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Receive_IT( this->ptr_hi2c_, DevAddress, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_transmit_it( uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Transmit_IT( this->ptr_hi2c_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_receive_it( uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Receive_IT( this->ptr_hi2c_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::master_transmit_dma( uint16_t DevAddress, uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Transmit_DMA( this->ptr_hi2c_, DevAddress, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::master_receive_dma( uint16_t DevAddress, uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Master_Receive_DMA( this->ptr_hi2c_, DevAddress, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_transmit_dma( uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Receive_DMA( this->ptr_hi2c_, pData, Size);
+    return HAL_ERROR;
+}
+
+HAL_StatusTypeDef Hard_I2C::slave_receive_dma( uint8_t *pData, uint16_t Size)
+{
+    if( this->ptr_hi2c_ )
+        HAL_I2C_Slave_Receive_DMA( this->ptr_hi2c_, pData, Size);
+    return HAL_ERROR;
+}
+
 
 void Hard_I2C::init(void)
 {
-    if( this->hi2c_ == nullptr )
+    if( this->ptr_hi2c_ == nullptr )
         return;
 }
 
-int Hard_I2C::write( uint8_t dev_addr, uint8_t reg_addr,
+uint8_t Hard_I2C::write( uint8_t dev_addr, uint8_t reg_addr,
     uint8_t data_len, uint8_t const* data_buf)
 {
-    if( this->hi2c_ == nullptr )
+    if( this->ptr_hi2c_ == nullptr )
         return I2C_STATE_::I2C_BUS_ERROR_;
+    HAL_I2C_Master_Transmit( this->ptr_hi2c_, dev_addr, data_buf, data_len, HAL_MAX_DELAY);
+    return 0x00;
 }
-int Hard_I2C::read( uint8_t dev_addr, uint8_t reg_addr,
+
+uint8_t Hard_I2C::read( uint8_t dev_addr, uint8_t reg_addr,
     uint8_t data_len, uint8_t* data_buf)
 {
-    if( this->hi2c_ == nullptr )
+    if( this->ptr_hi2c_ == nullptr )
         return I2C_STATE_::I2C_BUS_ERROR_;
+    HAL_I2C_Master_Receive( this->ptr_hi2c_, dev_addr, data_buf, data_len, HAL_MAX_DELAY);
+    return 0x00;
 }
 
 }
