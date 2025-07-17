2922458605bb596b2828dbbee4919ebfd3f34a6b
CYA <3155414334@qq.com>
2025-07-17 11:43:48 +0800

    feat:新增基础的超时功能

--- inc/modules/hc_sr04.hpp
+++ inc/modules/hc_sr04.hpp
@@ -10,11 +10,20 @@ private:
     const peripheral::gpio::Pin trig_;
     const peripheral::tim::IC ic_direct_;   //  上升沿检测
     const peripheral::tim::IC ic_indirect_; //  下降沿检测
+
+    float up_edge_;
+    float down_edge_;
 public:
     Hc_Sr04( const peripheral::gpio::Pin& trig, const peripheral::tim::IC& ic_direct, const peripheral::tim::IC& ic_indirect);
+    TIM_HandleTypeDef* handle(void);
 
     void init(void);
-    uint16_t get_distance(void);
+
+    uint32_t get_up_edge_cnt(void);
+    uint32_t get_down_edge_cnt(void);
+
+    //  返回值单位为 m
+    float get_distance( uint32_t timeout );
 };
 
 }
--- src/modules/hc_sr04.cpp
+++ src/modules/hc_sr04.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 #include "../../inc/modules/hc_sr04.hpp"
+#include "../../inc/modules/delay_timer.hpp"
+#include <cmath>
 
 namespace cya::module{
 
@@ -7,6 +9,11 @@ Hc_Sr04::Hc_Sr04( const peripheral::gpio::Pin& trig, const peripheral::tim::IC& 
     : trig_(trig), ic_direct_(ic_direct), ic_indirect_(ic_indirect)
 {
     this->init();
+}
+
+TIM_HandleTypeDef* Hc_Sr04::handle(void)
+{
+    return ic_direct_.handle();
 }
 
 void Hc_Sr04::init(void)
@@ -19,13 +26,45 @@ void Hc_Sr04::init(void)
     this->ic_indirect_.start_it();
 }
 
-uint16_t Hc_Sr04::get_distance(void)
+uint32_t Hc_Sr04::get_up_edge_cnt(void)
 {
+    this->up_edge_ = this->ic_direct_.get_ccr();
+    return this->up_edge_;
+}
+
+uint32_t Hc_Sr04::get_down_edge_cnt(void)
+{
+    this->down_edge_ = this->ic_indirect_.get_ccr();
+    return this->down_edge_;
+}
+
+//  返回值单位为 m
+//  timeout 每加一，约等于在 SYSCLK == 168MHz 的情况下增加最大超时 1.1us
+float Hc_Sr04::get_distance( uint32_t timeout )
+{
+    this->up_edge_ = 0;
+    this->down_edge_ = 0;
+
     this->trig_.set();
     //  延时 40~50 us
-
+    delay_us(45);
     this->trig_.reset();
     this->ic_direct_.set_cnt(0);
+
+    while( !(this->up_edge_) || !(this->down_edge_) )
+    {
+        if( timeout != 0xFFFFFFFFU )
+        {
+            --timeout;
+            if( !timeout )
+                return std::numeric_limits<float>::infinity();
+        }
+    }
+
+    //  n(us)  *  0.00034m/us = distance(m)
+    return ( ( this->down_edge_ - this->up_edge_ ) * 0.00034 ) / 2;
 }
+
+
 
 }
